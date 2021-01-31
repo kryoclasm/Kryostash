88a7c809be102a774112947bcfc3de767686cf1f
Shawn Rickman <mr.alaskan@gmail.com>
2021-01-30 17:18:57 -0800

    almost done

--- Go Projects/codeAcademy/heist.go
+++ Go Projects/codeAcademy/heist.go
@@ -27,10 +27,43 @@ func main() {
 	fmt.Println(isHeistOn, eludedGuards, openedVault)
 
 	if isHeistOn && openedVault >= 70 {
-		fmt.Print("Grab and GO!")
+		fmt.Println("Grab and GO!")
 	} else if isHeistOn && openedVault < 70 {
 		isHeistOn = false
 		fmt.Print("The vault cannot be opened.")
 	}
 
+	var leftSafely int
+	leftSafely = rand.Intn(5)
+
+	if isHeistOn {
+		switch leftSafely {
+
+		case 0:
+			isHeistOn = false
+			fmt.Print("The bank doors auto locked you inside!")
+
+		case 1:
+			isHeistOn = false
+			fmt.Print("Turns out vault doors don't open from the inside...")
+
+		case 2:
+			isHeistOn = false
+			fmt.Print("Lucky guard guard got the drop on you!")
+
+		case 3:
+			isHeistOn = false
+			fmt.Print("The getaway car got towed!")
+
+		case 4:
+			isHeistOn = true
+			fmt.Println("Start the getaway car, we are outta here!")
+		}
+	}
+
+	if isHeistOn {
+		var amtStolen int
+		amtStolen = 10000 + rand.Intn(1000000)
+		fmt.Println("We stole $", amtStolen)
+	}
 }
